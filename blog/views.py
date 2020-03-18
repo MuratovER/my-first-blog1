d9dfca82da2fe31d80f5fb4ccb0c18389573b673
MuratovER <cooperative.entr@gmail.com>
2020-03-18 09:30:48 +0300

    Adde views

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -25,7 +24,6 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
